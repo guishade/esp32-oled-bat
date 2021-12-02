651027762debace1010e2e58208c1affc399b22e
guishade <gui.shade@gmail.com>
2021-12-02 01:35:39 -0300

    gg

--- mpu9250-001/src/main.cpp
+++ mpu9250-001/src/main.cpp
@@ -1,9 +1,101 @@
 #include <Arduino.h>
+#include "MPU9250.h"
+#include <ArduinoOSCWiFi.h>
+#include "SSD1306Wire.h"
 
-void setup() {
-  // put your setup code here, to run once:
+const char *ssid = "test001";
+const char *pwd = "worktest";
+// const IPAddress ip(192, 168, 0, 201);
+// const IPAddress gateway(192, 168, 0, 1);
+// const IPAddress subnet(255, 255, 255, 0);
+const char *host = "192.168.0.140"; // destino
+const int publish_port = 1234;
+
+#define I2C_SDA 5//22
+#define I2C_SCL 4//21
+
+MPU9250 mpu;
+SSD1306Wire display(0x3c, I2C_SDA, I2C_SCL); 
+String rpy;
+float roll, pitch, yaw;
+
+void print_roll_pitch_yaw()
+{
+  Serial.print("Yaw, Pitch, Roll, Temp: ");
+  Serial.print(mpu.getYaw(), 2);
+  Serial.print(", ");
+  Serial.print(mpu.getPitch(), 2);
+  Serial.print(", ");
+  Serial.println(mpu.getRoll(), 2);
+  Serial.print(", ");
+  Serial.println(mpu.getTemperature(), 2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void send_osc()
+{
+  roll = mpu.getRoll();
+  pitch = mpu.getPitch();
+  yaw = mpu.getYaw();
+  rpy = "r" + String(roll) + "," + "p" + String(pitch) + "," + "y" + String(yaw);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  WiFi.disconnect(true, true);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pwd);
+  // WiFi.config(ip, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("WiFi connected, IP = ");
+  Serial.println(WiFi.localIP());
+
+  OscWiFi.publish(host, publish_port, "/v", rpy)
+      ->setFrameRate(60.f);
+
+  if (!mpu.setup(0x68))
+  {
+    while (1)
+    {
+      Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+      delay(5000);
+    }
+  }
+
+  display.init();
+  //display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+}
+
+long timeSinceLastModeSwitch = 0;
+
+void loop()
+{
+
+  if (mpu.update())
+  {
+    static uint32_t prev_ms = millis();
+    if (millis() > prev_ms + 25)
+    {
+      print_roll_pitch_yaw();
+      OscWiFi.update();
+      send_osc();
+      display.clear();
+      display.drawString(0, 0, rpy);
+      display.drawString(0, 0, rpy);
+      display.display();
+      prev_ms = millis();
+    }
+  }
 }
